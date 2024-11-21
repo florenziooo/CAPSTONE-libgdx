51b6e71d02477be2a039a480f2062bdb321d075f
smoll <carlpepino902@gmail.com>
2024-11-21 23:43:04 +0800

    added .gitignore

--- lwjgl3/build/resources/main/PixelMaps/TileSets16x16/Modern_Exteriors_Complete_Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Modern_Exteriors_Complete_Tileset" tilewidth="16" tileheight="16" tilecount="69344" columns="176">
- <image source="../Tiles/Modern_Exteriors_Complete_Tileset.png" width="2816" height="6304"/>
-</tileset>
--- lwjgl3/build/resources/main/PixelMaps/TileSets16x16/asdasd.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="asdasd" tilewidth="16" tileheight="16" tilecount="8" columns="2">
- <image source="../Tiles/asdasd.png" width="32" height="64"/>
-</tileset>
