a11d0ba4e539410df1b5eb0bb33f3662ebebdd11
smoll <carlpepino902@gmail.com>
2024-11-27 19:17:47 +0800

    added main menu ui

--- core/src/main/java/io/mygame/screens/MainMenu.java
+++ core/src/main/java/io/mygame/screens/MainMenu.java
@@ -2,15 +2,30 @@ package io.mygame.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenu extends WildCatScreen {
-    Texture background;
-    SpriteBatch batch;
-    float time = 0;
+    private Texture background;
+    private SpriteBatch batch;
+    private Stage stage;
+    private Table table;
+    private Skin redSkin;
+    private Skin yellowSkin;
+
+    private TextButton startButton;
+    private TextButton loadButton;
+    private TextButton exitButton;
+
+    private float time = 0;
 
     public MainMenu(Game game) {
         super(game);
@@ -20,21 +35,51 @@ public class MainMenu extends WildCatScreen {
     public void show () {
         background = new Texture("images/main_menu_bg.png");
         batch = new SpriteBatch();
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        table = new Table();
+        table.setY(-250);
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        yellowSkin = new Skin(Gdx.files.internal("skins/main_menu_button/yellow/yellow.json"));
+        redSkin = new Skin(Gdx.files.internal("skins/main_menu_button/red/red.json"));
+
+        startButton = new TextButton("START GAME", yellowSkin);
+        table.add(startButton).space(15f);
+        table.row();
+
+        loadButton = new TextButton("LOAD GAME", yellowSkin);
+        table.add(loadButton).space(15f);
+        table.row();
+
+        exitButton = new TextButton("EXIT GAME", redSkin);
+        table.add(exitButton).space(15f);
+
+        startButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
     }
 
     @Override
     public void render (float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
 
-        time += delta;
+        stage.act();
+        stage.draw();
+    }
 
-        if(time > 1) {
-            if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY) || Gdx.input.justTouched()) {
-                game.setScreen(new GameScreen(game));
-            }
-        }
+    @Override
+    public void dispose() {
+        stage.dispose();
+        batch.dispose();
     }
 }
