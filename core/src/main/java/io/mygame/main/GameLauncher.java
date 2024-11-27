fbab4086d8caee51b2a39d7d178510cf87d2ff1c
smoll <carlpepino902@gmail.com>
2024-11-27 23:43:51 +0800

    added cute cursor

--- core/src/main/java/io/mygame/main/GameLauncher.java
+++ core/src/main/java/io/mygame/main/GameLauncher.java
@@ -1,12 +1,20 @@
 package io.mygame.main;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Pixmap;
 import io.mygame.screens.MainMenu;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class GameLauncher extends Game {
     @Override
     public void create() {
+        Pixmap pixmap = new Pixmap(Gdx.files.internal("cursor/cursor.png"));
+        Cursor customCursor = Gdx.graphics.newCursor(pixmap, 4, 4);
+        Gdx.graphics.setCursor(customCursor);
+        pixmap.dispose();
+
         setScreen(new MainMenu(this));
     }
 }
