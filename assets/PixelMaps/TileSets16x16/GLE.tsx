ba53f2328ac9074de39d79dfb8400ec213a47b4c
Marsh <raimarshaun123@gmail.com>
2024-12-03 03:02:05 +0800

    Finished Layout Just the assets left

---
+++ assets/PixelMaps/TileSets16x16/GLE.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="GLE" tilewidth="16" tileheight="16" tilecount="506" columns="11">
+ <image source="../Tiles/GLE.png" width="176" height="736"/>
+</tileset>
